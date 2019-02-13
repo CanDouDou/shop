95574e29f4e4cd1eb02dbdf51e3e08b874b803f8
ZrZkao <523757299@qq.com>
2019-02-13 15:39:06 +0800

    登录-布局

---
+++ src/components/login.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <el-form :label-position="labelPosition" label-width="80px">
+            <el-form-item label="名字">
+                <el-input v-model="formdata.username"></el-input>
+            </el-form-item>
+            <el-form-item label="密码">
+                <el-input v-model="formdata.password"></el-input>
+            </el-form-item>
+        </el-form>
+    </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      labelPosition: 'top',
+      formdata: {
+        name: '',
+        password: ''
+      }
+    }
+  }
+}
+</script>
+
+<style>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
+import Login from '../components/login.vue'
+import Home from '../components/home.vue'
+
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
+      path: '/login',
+      component: Login
+    },
+    {
       path: '/',
-      component: ''
+      component: Home
     }
   ]
 })
