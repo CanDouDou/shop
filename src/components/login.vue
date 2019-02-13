11359958dc7ab6379a84a508b29f812a60972668
ZrZkao <523757299@qq.com>
2019-02-13 16:17:30 +0800

    登录-样式

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,13 +1,16 @@
 <template>
-    <div>
-        <el-form :label-position="labelPosition" label-width="80px">
+    <div class="login">
+        <el-form class="login-form" :label-position="labelPosition" label-width="80px">
+            <h2>用户登录</h2>
             <el-form-item label="名字">
                 <el-input v-model="formdata.username"></el-input>
             </el-form-item>
             <el-form-item label="密码">
                 <el-input v-model="formdata.password"></el-input>
             </el-form-item>
+            <el-button class="login-sub" type="primary">登录</el-button>
         </el-form>
+        
     </div>
 </template>
 
@@ -26,4 +29,23 @@ export default {
 </script>
 
 <style>
+html,body{
+    height: 100%;
+}
+.login{
+    height: 100%;
+    background-color: #324152;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.login-form {
+    border-radius: 5px;
+    padding: 30px;
+    width: 400px;
+    background-color: #fff;
+}
+.login-form .login-sub {
+    width: 100%;
+}
 </style>
